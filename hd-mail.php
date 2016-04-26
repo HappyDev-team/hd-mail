e76ec3ab7d3ae366a687490ea78c30fefc684ee6
JonathanBraley <jonathan.b@happy-dev.fr>
2016-04-26 21:39:37 +0200

    Feature: receiving and sending mail for Happy-dev webSite

---
+++ hd-mail.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin Name: HD MAIL
+ * Description: This is a test for sending via ajax and wordpress wp-mail for the Happy-Dev website.
+ * Author: Jonathan BRALEY
+ * License: GPL2
+ */
+ 
+ add_action("wp_ajax_send_HD_mail","HD_wp_ajax_send_mail");
+ add_action("wp_ajax_nopriv_send_HD_mail","HD_wp_ajax_send_mail");
+ 
+ function HD_wp_ajax_send_mail(){
+	 if(isset($_POST["name"]) && isset($_POST["howru"]) && isset($_POST["request"]) && isset($_POST["contact"]) && isset($_POST["mails"]) && isset($_POST["action"])){
+$message= "Bonjour Happy Dev,
+
+".$_POST["name"]." cherche à vous contacter.
+
+Lors de sa demande il : ".$_POST["howru"]."
+
+Sujet de la demande:
+
+".$_POST["request"]."
+
+Vous pourrez le joindre de cette manière : ".$_POST["contact"]."
+
+Bonne journée!";
+		 
+		 $res = wp_mail($_POST["mails"],$_POST["name"]." cherche à vous contacter",$message);
+		 
+		 echo res;
+	 }else{
+		 echo -1;
+	 }
+ }
+ 
+ ?>
